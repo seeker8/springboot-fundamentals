0df8f7b7eb46bb98f0f5bfca5dfcac9160069705
seeker8 <chuyo_gk87@yahoo.com.mx>
2026-04-28 18:07:59 -0400

    add CommandLineRunner bean insert data to the database

--- src/main/java/com/reilly/shopping/ShoppingApplication.java
+++ src/main/java/com/reilly/shopping/ShoppingApplication.java
@@ -1,13 +1,33 @@
 package com.reilly.shopping;
 
+import com.reilly.shopping.dao.ProductRepository;
+import com.reilly.shopping.entities.Product;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @SpringBootApplication
 public class ShoppingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ShoppingApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ShoppingApplication.class, args);
+  }
+
+  @Bean
+  public CommandLineRunner initializeDatabase(ProductRepository productRepository) {
+    return args -> {
+      if (productRepository.count() == 0) {
+        productRepository.saveAll(List.of(
+          new Product("Ink Wand", BigDecimal.valueOf(3.99)),
+          new Product("Conjuring Ink", BigDecimal.valueOf(3.99)),
+          new Product("Palm Quire", BigDecimal.valueOf(3.99))
+        )).forEach(System.out::println);
+      }
+    };
+  }
 
 }
